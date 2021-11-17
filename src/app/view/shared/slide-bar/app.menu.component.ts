1df6f301aed733bc826912a9bbf0a1907d5accf4
tachrimant <tachrimantsaad@gmail.com>
2021-11-17 22:33:26 +0100

    gadit homework

--- src/app/view/shared/slide-bar/app.menu.component.ts
+++ src/app/view/shared/slide-bar/app.menu.component.ts
@@ -105,7 +105,6 @@ export class AppMenuComponent implements OnInit {
                     {label: 'Schedule', icon: 'pi pi-fw pi-calendar-times', routerLink: ['/prof/schedule']},
                     {label: 'Student connected', icon: 'pi pi-fw pi-calendar-times', routerLink: ['/prof/connected-student']},
                     {label: 'News', icon: 'pi pi-fw pi-clock', routerLink: ['/prof/news-teacher']},
-                    {label: 'Student Review', icon: 'pi pi-fw pi-comments', routerLink: ['/prof/comment-review']},
                     {label: 'Student HomeWork', icon: 'pi pi-sliders-h', routerLink: ['/prof/sudents-homework']},
                     {label: 'FAQ', icon: 'pi pi-fw pi-question-circle', routerLink: ['/prof/faq-teacher']},
                 ];
