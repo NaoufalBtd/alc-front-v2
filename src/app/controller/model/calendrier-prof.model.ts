5c0fe10cc54ff360606012dcf0c5f5f350f19475
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2021-12-26 17:05:26 +0100

    commit-youssef: Quiz student + schedule

--- src/app/controller/model/calendrier-prof.model.ts
+++ src/app/controller/model/calendrier-prof.model.ts
@@ -5,11 +5,13 @@ import {Cours} from './cours.model';
 
 export class ScheduleProf {
     public id = Number(0);
-    subject: string;
+    public subject: string;
     public ref: string;
     public startTime: Date = new Date();
     public endTime: Date = new Date();
     public groupeEtudiant = new GroupeEtudiant();
+    public grpName: string;
+    public profName: string;
     public prof = new Prof();
     public cours: Cours = new Cours();
 }
