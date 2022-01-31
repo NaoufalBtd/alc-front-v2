025ff95844620deef284c5aeade3abc872dee0de
tachrimant <tachrimantsaad@gmail.com>
2022-01-31 22:25:25 +0100

    total students for each pack added + some style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -266,6 +266,7 @@ import {ViewProfilEtudiantComponent} from './view/prof/synthese-session/view-pro
 import {SalarylistComponent} from './view/admin/salarylist/salarylist.component';
 import {ScheduleProfComponent} from './view/admin/professeur/schedule-prof/schedule-prof.component';
 import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
