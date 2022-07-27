307cf0c7836f255ecd15cf36f2d22b105ffd2ab7
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-07-27 13:04:14 +0100

    contact us + fix bugs

---
+++ src/app/controller/service/contact.service.ts
@@ -0,0 +1,41 @@
+/* tslint:disable:variable-name */
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {Contact} from '../model/contact.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ContactService {
+    adminUrl = environment.adminUrl;
+    private _displayContactMessage: boolean;
+
+
+    constructor(private http: HttpClient) {
+    }
+
+
+    get displayContactMessage(): boolean {
+        return this._displayContactMessage;
+    }
+
+    set displayContactMessage(value: boolean) {
+        this._displayContactMessage = value;
+    }
+
+    public findAll(): Observable<Array<Contact>> {
+        return this.http.get<Array<Contact>>(this.adminUrl + 'contact/');
+    }
+
+
+    public findByEmail(email: string): Observable<Contact[]> {
+        return this.http.get<Contact[]>(this.adminUrl + 'contact/email/' + email);
+    }
+
+    public save(contact: Contact): Observable<Contact> {
+        return this.http.post<Contact>(this.adminUrl + 'contact/', contact);
+    }
+
+}
--- src/app/view/public/home-principale/contact-us/contact-us.component.ts
+++ src/app/view/public/home-principale/contact-us/contact-us.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContactService} from '../../../../controller/service/contact.service';
+import {Contact} from '../../../../controller/model/contact.model';
+import {MessageService} from 'primeng/api';
 
 @Component({
-  selector: 'app-contact-us',
-  templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+    selector: 'app-contact-us',
+    templateUrl: './contact-us.component.html',
+    styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+    contact: Contact = new Contact();
 
-  constructor() { }
+    constructor(private contactService: ContactService, private messageService: MessageService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    save(): void {
+        this.contact.setFrom = 'ANONYMOUS';
+        this.contact.date = new Date();
+        this.contactService.save(this.contact).subscribe(
+            data => {
+                this.messageService.add({
+                    severity: 'success',
+                    detail: 'Message sent successfully ',
+                    life: 4000
+                });
+                this.contact = new Contact();
+            }, error => {
+                this.messageService.add({
+                    severity: 'error',
+                    detail: 'Something went wrong, please try again.',
+                    life: 4000
+                });
+            }
+        );
+    }
+
 
 }
