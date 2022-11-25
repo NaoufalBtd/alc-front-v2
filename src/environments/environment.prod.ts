b5ff304b3a626e775251ac7fece579c41809da5f
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-11-25 15:46:21 +0100

    fix header

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
-// const url = 'http://localhost:8036';
 const url = 'https://engflexy.ma';
-// const socket = 'ws://localhost:8036/chat';
-// const socket = 'ws://179.61.219.179:8036/chat';
-// const socket = 'wss://engflexy.ma:8036/chat';
 const socket = 'wss://engflexy.ma/chat';
 
 export const environment = {
