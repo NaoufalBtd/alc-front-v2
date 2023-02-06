13f100682820dd357d46ad6545c3ab3bcc9c6998
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2023-02-06 01:22:15 +0100

    fix sync bugs fix size of session in websocket

--- src/app/controller/service/web-socket.service.ts
+++ src/app/controller/service/web-socket.service.ts
@@ -490,7 +490,7 @@ export class WebSocketService {
                     if (this.webSocket.readyState === this.webSocket.OPEN) {
                         this.webSocket.send(JSON.stringify(chatMessageDto));
                     }
-                }
+                };
             }
         }
     }
